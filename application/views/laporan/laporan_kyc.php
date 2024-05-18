fb257f49c539b7bed7b4e3011f5c6a000ed48849
Rizqi Adnan <rizkyadnan23@gmail.com>
2024-05-18 16:50:45 +0700

    edit apexcharts

--- application/views/laporan/laporan_kyc.php
+++ application/views/laporan/laporan_kyc.php
@@ -70,20 +70,87 @@ $jsonData = json_encode($data);
 				<script>
 					document.addEventListener('DOMContentLoaded', function () {
 						var phpData = <?php echo $jsonData; ?>;
-
+						
 						var options = {
-							chart: {
-								type: 'line',
-								height: 350
-							},
 							series: [{
-								name: 'Example Data',
-								data: phpData
+								name: 'XYZ MOTORS',
+								data: dates
 							}],
-							xaxis: {
-								categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep']
+							chart: {
+								type: 'area',
+								stacked: false,
+								height: 350,
+							zoom: {
+								type: 'x',
+								enabled: true,
+								autoScaleYaxis: true
+							},
+							toolbar: {
+								autoSelected: 'zoom'
 							}
-						};
+							},
+							dataLabels: {
+								enabled: false
+							},
+							markers: {
+								size: 0,
+							},
+							title: {
+								text: 'Stock Price Movement',
+								align: 'left'
+							},
+							fill: {
+								type: 'gradient',
+								gradient: {
+									shadeIntensity: 1,
+									inverseColors: false,
+									opacityFrom: 0.5,
+									opacityTo: 0,
+									stops: [0, 90, 100]
+							},
+							},
+							yaxis: {
+							labels: {
+								formatter: function (val) {
+								return (val / 1000000).toFixed(0);
+								},
+							},
+							title: {
+								text: 'Price'
+							},
+							},
+							xaxis: {
+								type: 'datetime',
+							},
+							tooltip: {
+								shared: false,
+							y: {
+								formatter: function (val) {
+								return (val / 1000000).toFixed(0)
+								}
+							}
+						}
+					};
+						// var options = {
+						// 	chart: {
+						// 		type: 'line',
+						// 		height: 350
+						// 	},
+						// 	series: [{	
+						// 		name: 'Example Data',
+						// 		data: phpData
+						// 	}],
+						// 	xaxis: {
+						// 		categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep']
+						// 	},
+						// 	toolbar: {
+            			// 		autoSelected: 'zoom'
+          				// 	},
+						// 	title: {
+						// 		text: 'Laporan KYC',
+						// 		align: 'left'
+						// 	}
+						// };
 
 						var chart = new ApexCharts(document.querySelector("#chart"), options);
 						chart.render();
